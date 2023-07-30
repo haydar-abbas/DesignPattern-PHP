8f9909accd86dc7c11909a96bf798cd40626cdca
haydar-abbas <haydarabbas40@proton.me>
2023-07-30 08:49:59 +0300

    Update Decorator Pattern

--- structural/Decorator/index.php
+++ structural/Decorator/index.php
@@ -2,18 +2,18 @@
 
 require_once 'vendor/autoload.php';
 
-$sandwich1 = new \src\BasicSandwich();
-echo "Description: " . $sandwich1->getDescription() . "\n";
-echo "Cost: " .$sandwich1->getCost() . "\n";
+$sandwich = new \src\BasicSandwich();
+echo "Description: " . $sandwich->getDescription() . "\n";
+echo "Cost: " .$sandwich->getCost() . "\n";
 
-$sandwich2 = new src\Beef($sandwich1);
-echo "Description: " . $sandwich2->getDescription() . "\n";
-echo "Cost: " .$sandwich2->getCost() . "\n";
+$sandwich = new src\Beef($sandwich);
+echo "Description: " . $sandwich->getDescription() . "\n";
+echo "Cost: " .$sandwich->getCost() . "\n";
 
-$sandwich3 = new \src\Chees($sandwich2);
-echo "Description: " . $sandwich3->getDescription() . "\n";
-echo "Cost: " .$sandwich3->getCost() . "\n";
+$sandwich = new \src\Chees($sandwich);
+echo "Description: " . $sandwich->getDescription() . "\n";
+echo "Cost: " .$sandwich->getCost() . "\n";
 
-$sandwich4 = new \src\Salt($sandwich3);
-echo "Description: " . $sandwich4->getDescription() . "\n";
-echo "Cost: " .$sandwich4->getCost() . "\n";
+$sandwich = new \src\Salt($sandwich);
+echo "Description: " . $sandwich->getDescription() . "\n";
+echo "Cost: " .$sandwich->getCost() . "\n";
