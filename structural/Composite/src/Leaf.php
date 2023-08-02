e131ebb8d9cebbb8ab023fedc215ed02f3a6cd2b
haydar-abbas <haydarabbas40@proton.me>
2023-08-02 17:51:15 +0300

    Update Composite Pattern

--- structural/Composite/src/Leaf.php
+++ structural/Composite/src/Leaf.php
@@ -2,18 +2,19 @@
 
 namespace src;
 
-class Leaf implements Component {
-
+class Leaf implements Component
+{
     private String $name;
     private int $price;
 
-    public function __construct(String $name, int $price) {
+    public function __construct(String $name, int $price)
+    {
         $this->name = $name;
         $this->price = $price;
     }
 
-    public function showPrice(): void {
-        echo $this->name . " : " . $this->price . "\n";
+    public function showPrice(): void
+    {
+        printf("%s : %s\n", $this->name, $this->price);
     }
-
 }
