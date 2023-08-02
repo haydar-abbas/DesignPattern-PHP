648f2867fba362a0f0f1408055c506b44a35f934
haydar-abbas <haydarabbas40@proton.me>
2023-08-02 17:43:00 +0300

    Update Prototype Pattern

--- creational/Prototype/index.php
+++ creational/Prototype/index.php
@@ -1,12 +1,14 @@
 <?php
 
+use src\Animal;
+use src\Human;
+
 require 'vendor/autoload.php';
 
-$human1 = new \src\Human("Haydar", 33, new src\Animal("fifo", "cat"));
+$human1 = new Human("Haydar", 33, new Animal("fifo", "cat"));
 var_dump($human1);
 
 $human2 = $human1->createClone();
 $human2->setName("Abbas");
-$human2->setAnimal(new src\Animal("lifo", "dog"));
+$human2->setAnimal(new Animal("lifo", "dog"));
 var_dump($human2);
-    
--- creational/Prototype/src/Human.php
+++ creational/Prototype/src/Human.php
@@ -2,32 +2,36 @@
 
 namespace src;
 
-class Human implements IPrototype {
-
-    private $name;
-    private $age;
+class Human implements IPrototype
+{
+    private string $name;
+    private int $age;
     private Animal $animal;
 
-    public function __construct($name, $age, Animal $animal) {
+    public function __construct(string $name, int $age, Animal $animal)
+    {
         $this->name = $name;
         $this->age = $age;
         $this->animal = $animal;
     }
 
-    public function setName($name): void {
+    public function setName($name): void
+    {
         $this->name = $name;
     }
 
-    public function setAge($age): void {
+    public function setAge($age): void
+    {
         $this->age = $age;
     }
 
-    public function setAnimal(Animal $animal): void {
+    public function setAnimal(Animal $animal): void
+    {
         $this->animal = $animal;
     }
 
-    public function createClone() {
+    public function createClone()
+    {
         return new Human($this->name, $this->age, $this->animal);
     }
-
 }
