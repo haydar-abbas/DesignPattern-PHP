de367d81f609e0646a74cb2870448a070ac3890d
haydar-abbas <haydarabbas40@proton.me>
2023-08-14 09:21:34 +0300

    Update Facade Pattern

--- structural/Facade/src/BankAccountFacade.php
+++ structural/Facade/src/BankAccountFacade.php
@@ -2,37 +2,42 @@
 
 namespace src;
 
-class BankAccountFacade {
-
+class BankAccountFacade
+{
     private int $accountNumber;
     private int $securityCode;
+
     private AccountNumberCheck $acct_checker;
     private SecurityCodeCheck $code_checker;
     private FundsCheck $fund_checker;
-    private WelcomeToBank $bank_welcome;
 
-    public function __construct(int $accountNumber, int $securityCode) {
+    public function __construct(int $accountNumber, int $securityCode)
+    {
         $this->accountNumber = $accountNumber;
         $this->securityCode = $securityCode;
 
-        $this->bank_welcome = new WelcomeToBank();
+        new WelcomeToBank();
         $this->acct_checker = new AccountNumberCheck();
         $this->code_checker = new SecurityCodeCheck();
         $this->fund_checker = new FundsCheck();
     }
 
-    public function getAccountNumber(): int {
+    public function getAccountNumber(): int
+    {
         return $this->accountNumber;
     }
 
-    public function getSecurityCode(): int {
+    public function getSecurityCode(): int
+    {
         return $this->securityCode;
     }
 
-    public function withDrawCash(float $cashToGet): void {
-        if ($this->acct_checker->accountActive($this->getAccountNumber()) &&
-                $this->code_checker->isCodeCorrect($this->getSecurityCode()) &&
-                $this->fund_checker->haveEnoughMoney($cashToGet)
+    public function withDrawCash(float $cashToGet): void
+    {
+        if (
+            $this->acct_checker->accountActive($this->getAccountNumber()) &&
+            $this->code_checker->isCodeCorrect($this->getSecurityCode()) &&
+            $this->fund_checker->haveEnoughMoney($cashToGet)
         ) {
             $this->fund_checker->makeWithDraw($cashToGet);
             echo "Transaction complete\n\n";
@@ -41,9 +46,11 @@ class BankAccountFacade {
         }
     }
 
-    public function depositCash(float $cashToDeposit): void {
-        if ($this->acct_checker->accountActive($this->getAccountNumber()) &&
-                $this->code_checker->isCodeCorrect($this->getSecurityCode())
+    public function depositCash(float $cashToDeposit): void
+    {
+        if (
+            $this->acct_checker->accountActive($this->getAccountNumber()) &&
+            $this->code_checker->isCodeCorrect($this->getSecurityCode())
         ) {
             $this->fund_checker->makeDeposit($cashToDeposit);
             echo "Transaction complete\n\n";
